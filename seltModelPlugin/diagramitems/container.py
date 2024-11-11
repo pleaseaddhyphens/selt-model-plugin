ce59a21873f01fdb2fb860b7826a4767694483e8
Tikhonenko, Valentin <Valentin.Tikhonenko@skoltech.ru>
2024-11-11 11:32:28 +0300

    indent fix

--- seltModelPlugin/diagramitems/container.py
+++ seltModelPlugin/diagramitems/container.py
@@ -1,49 +1,49 @@
-    from gaphor import UML
-    from seltModelPlugin import c4model
-    from gaphor.diagram.presentation import ElementPresentation, Named, text_name
-    from gaphor.diagram.shapes import Box, CssNode, Text, draw_border
-    from gaphor.diagram.support import represents
-    from gaphor.UML.compartments import text_stereotypes
+from gaphor import UML
+from seltModelPlugin import c4model
+from gaphor.diagram.presentation import ElementPresentation, Named, text_name
+from gaphor.diagram.shapes import Box, CssNode, Text, draw_border
+from gaphor.diagram.support import represents
+from gaphor.UML.compartments import text_stereotypes
 
 
-    @represents(c4model.C4Container)
-    class C4ContainerItem(Named, ElementPresentation):
-        def __init__(self, diagram, id=None):
-            super().__init__(diagram, id)
+@represents(c4model.C4Container)
+class C4ContainerItem(Named, ElementPresentation):
+    def __init__(self, diagram, id=None):
+        super().__init__(diagram, id)
 
-            self.watch("subject.name")
-            self.watch("subject[C4Container].description")
-            self.watch("subject.appliedStereotype.classifier.name")
+        self.watch("subject.name")
+        self.watch("subject[C4Container].description")
+        self.watch("subject.appliedStereotype.classifier.name")
 
-            self.watch("children", self.update_shapes)
+        self.watch("children", self.update_shapes)
 
-        def update_shapes(self, event=None):
-            self.shape = Box(
-                text_stereotypes(
-                    self,
-                    lambda: [self.diagram.gettext("profile")]
-                    if isinstance(self.subject, UML.Profile)
-                    else [],
+    def update_shapes(self, event=None):
+        self.shape = Box(
+            text_stereotypes(
+                self,
+                lambda: [self.diagram.gettext("profile")]
+                if isinstance(self.subject, UML.Profile)
+                else [],
+            ),
+            text_name(self),
+            CssNode(
+                "technology",
+                self.subject,
+                Text(
+                    text=lambda: self.subject.technology
+                    and f"[{self.subject.technology}]"
                 ),
-                text_name(self),
-                CssNode(
-                    "technology",
-                    self.subject,
-                    Text(
-                        text=lambda: self.subject.technology
-                        and f"[{self.subject.technology}]"
+            ),
+            *(
+                ()
+                if self.children
+                else (
+                    CssNode(
+                        "description",
+                        self.subject,
+                        Text(text=lambda: self.subject.description or ""),
                     ),
-                ),
-                *(
-                    ()
-                    if self.children
-                    else (
-                        CssNode(
-                            "description",
-                            self.subject,
-                            Text(text=lambda: self.subject.description or ""),
-                        ),
-                    )
-                ),
-                draw=draw_border,
-            )
+                )
+            ),
+            draw=draw_border,
+        )
